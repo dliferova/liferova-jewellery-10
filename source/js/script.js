003f7450cd7f410b6626155d7333f79635bffd2c
Daria Liferova <liferova.daria@gmail.com>
2022-02-16 20:59:34 +0100

    Добавлен JS для мобильного меню.

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,25 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const pageHeaderMenu = document.querySelector('.page-header__menu');
+const headerToggle = document.querySelector('.page-header__toggle');
+const navigationElement = document.querySelector('.page-header__navigation');
+const pageHeader = document.querySelector('.page-header');
+const pageBody = document.querySelector('.page__body');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+pageHeaderMenu.classList.remove('page-header__menu_nojs');
+pageHeaderMenu.classList.remove('page-header__menu_opened');
+navigationElement.classList.remove('page-header__navigation_opened');
+navigationElement.classList.add('page-header__navigation_closed');
+
+const screenFreeze = () => {
+  pageHeader.classList.toggle('page-header_opened-menu_view');
+  pageBody.classList.toggle('scroll-hidden');
+}
+
+headerToggle.addEventListener('click', () => {
+  pageHeaderMenu.classList.toggle('page-header__menu_opened');
+  navigationElement.classList.toggle('page-header__navigation_closed');
+  navigationElement.classList.toggle('page-header__navigation_opened');
+  screenFreeze();
 });
+
